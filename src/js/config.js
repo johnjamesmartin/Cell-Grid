4148a4e34f06efa390f9e56b7eda1d61f68cc442
John Martin <johnjamesmartin@live.com>
2019-08-19 01:11:23 +0100

    Modify comments II

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@
    the following properties:
  ************************************************************/
 const gridConfig = {
-  size: 12, // this number is squared to make up number of cells
+  size: 12, // number is squared to make up number of cells (144 in this case)
   scale: 0.8, // 0 - 1
   width: 750,
   border: {
